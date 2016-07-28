660ca891fba3a78af8ff585e269aad6b1d831690
Vivek Nagar <nagarvivek@yahoo.com>
2016-07-28 16:39:12 -0500

    Initial Commit

---
+++ FirstPersonShooter/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  FirstPersonShooter
+//
+//  Created by Vivek Nagar on 7/28/16.
+//  Copyright (c) 2016 Vivek Nagar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+    
+}
