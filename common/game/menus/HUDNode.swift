acf4d03a6d0e4c80458ed247d4bf89057f8428d7
Vivek Nagar <nagarvivek@yahoo.com>
2016-08-01 19:27:52 -0500

    Fix overlay screen health bar.

--- common/game/menus/HUDNode.swift
+++ common/game/menus/HUDNode.swift
@@ -9,94 +9,13 @@
 import SpriteKit
 import GameController
 
-#if os(iOS) || os(tvOS)
-    class ProgressView : UIView {
-        var strengthScore:Float = 0.0
-        
-        override init(frame frameRect: CGRect) {
-            super.init(frame:frameRect);
-        }
-        
-        required init?(coder: NSCoder) {
-            super.init(coder: coder)
-        }
-        
-        func update(strength:Float) {
-            if (strengthScore != strength) {
-                strengthScore = strength;
-                self.setNeedsDisplay()
-            }
-        }
-        
-        override func drawRect(dirtyRect:CGRect) {
-            let context = UIGraphicsGetCurrentContext ();
-            // The color to fill the rectangle (in this case black)
-            CGContextSetRGBFillColor(context, 0.5, 0.5, 0.5, 1.0);
-            // draw the filled rectangle
-            CGContextFillRect (context, self.bounds);
-            
-            var rect = CGRectInset(self.bounds, 1.0, 2.0)
-            if (strengthScore <= 0.2) {
-                CGContextSetRGBFillColor(context, 1.0, 0.0, 0.0, 1.0);
-
-            } else if (strengthScore <= 0.4) {
-                CGContextSetRGBFillColor(context, 1.0, 1.0, 0.0, 1.0);
-
-            } else {
-                CGContextSetRGBFillColor(context, 0.0, 1.0, 0.0, 1.0);
-            }
-            
-            rect.size.width = CGFloat(floor(Float(rect.size.width) * (strengthScore)))
-            //UIBezierPath.fillRect(rect)
-            // draw the filled rectangle
-            CGContextFillRect (context, rect);
-            
-            
-        }
-    }
-
-#elseif os(OSX)
-    class ProgressView : NSView {
-        var strengthScore:Float = 0.0
-
-        override init(frame frameRect: NSRect) {
-            super.init(frame:frameRect);
-        }
-        
-        required init?(coder: NSCoder) {
-            super.init(coder: coder)
-        }
-        
-        func update(strength:Float) {
-            if (strengthScore != strength) {
-                strengthScore = strength;
-                self.needsDisplay = true
-            }
-        }
-    
-        override func drawRect(dirtyRect:NSRect) {
-            NSColor.grayColor().set()
-            NSRectFill(self.bounds)
-            var rect = NSInsetRect(self.bounds, 1.0, 2.0)
-            if (strengthScore <= 0.2) {
-                NSColor.redColor().set()
-            } else if (strengthScore <= 0.4) {
-                NSColor.yellowColor().set()
-            } else {
-                NSColor.greenColor().set()
-            }
-    
-            rect.size.width = CGFloat(floor(Float(rect.size.width) * (strengthScore)))
-            NSBezierPath.fillRect(rect)
-            
-        }
-    }
-#endif
 
 class HUDNode : SKNode {
     var size = CGSizeZero
     var overlay:SKScene?
-    var healthBar:ProgressView?
+    var healthBar:SKSpriteNode?
+    let maxHealth = 100
+
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder:aDecoder)
@@ -110,16 +29,12 @@ class HUDNode : SKNode {
         self.addControls()
     }
     
-    func setHealth(value:Float) {
-        healthBar!.update(value)
+    func setHealth(value:Int) {
+        updateHealthBar(healthBar!, withHealthPoints: value)
     }
     
     private func addControls() {
-        guard let overlayScene = overlay else {
-            print("No overlay scene")
-            return
-        }
-
+        
         // The virtual D-pad
         #if os(iOS)
             let dpadSprite = SKSpriteNode(imageNamed: "art.scnassets/overlays/dpad")
@@ -135,30 +50,70 @@ class HUDNode : SKNode {
             attackSprite.zPosition = 1.0
             attackSprite.setScale(0.4)
             self.addChild(attackSprite)
+        #endif
             
-            let position = CGPointMake(size.width * 0.1, size.height*0.1)
-            self.healthBar = self.createHealthBar(position)
-            overlayScene.view!.addSubview(healthBar!)
-            self.setHealth(1.0)
-        #else
             let position = CGPointMake(size.width * 0.1, size.height*0.9)
             self.healthBar = self.createHealthBar(position)
-            overlayScene.view!.addSubview(healthBar!)
-            self.setHealth(1.0)
-        #endif
-
+            self.setHealth(40)
     }
     
     func removeControls() {
-    
+        healthBar!.hidden = true
     }
     
-    private func createHealthBar(position:CGPoint) -> ProgressView {
-        let healthView = ProgressView()
-        healthView.frame = CGRect(x: position.x, y: position.y, width: 80, height: 20)
-        return healthView
+
+
+    private func createHealthBar(position:CGPoint) -> SKSpriteNode {
+        let playerHealthBar = SKSpriteNode()
+        
+        addChild(playerHealthBar)
+        
+        playerHealthBar.position = CGPoint(x: position.x, y: position.y)
+        return playerHealthBar
     }
     
+    
+    private func updateHealthBar(node: SKSpriteNode, withHealthPoints hp: Int) {
+        let HealthBarWidth: CGFloat = 40
+        let HealthBarHeight: CGFloat = 10
+        let barSize = CGSize(width: HealthBarWidth, height: HealthBarHeight);
+        
+        let fillColor = SKColor(red: 113.0/255, green: 202.0/255, blue: 53.0/255, alpha:1)
+        let borderColor = SKColor(red: 35.0/255, green: 28.0/255, blue: 40.0/255, alpha:1)
+        
+        // create drawing context
+        #if os(iOS)
+        UIGraphicsBeginImageContextWithOptions(barSize, false, 0)
+        let context = UIGraphicsGetCurrentContext()
+        #else
+        let spriteImage = NSImage(size: NSMakeSize(HealthBarWidth, HealthBarHeight))
+        spriteImage.lockFocus()
+        let context = NSGraphicsContext.currentContext()?.CGContext
+        #endif
+        
+        // draw the outline for the health bar
+        borderColor.setStroke()
+        let borderRect = CGRect(origin: CGPointZero, size: barSize)
+        CGContextStrokeRectWithWidth(context, borderRect, 1)
+        
+        // draw the health bar with a colored rectangle
+        fillColor.setFill()
+        let barWidth = (barSize.width - 1) * CGFloat(hp) / CGFloat(maxHealth)
+        let barRect = CGRect(x: 0.5, y: 0.5, width: barWidth, height: barSize.height - 1)
+        CGContextFillRect(context, barRect)
+        
+        // extract image
+        #if os(iOS)
+        let spriteImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        #else
+        spriteImage.unlockFocus()
+        #endif
+        
+        // set sprite texture and size
+        node.texture = SKTexture(image: spriteImage)
+        node.size = barSize
+    }
     
     #if os(iOS)
     func virtualDPadBounds() -> CGRect {
